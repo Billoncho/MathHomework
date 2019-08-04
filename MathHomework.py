e67216a8894b40ecbe3ea5f20ef0655965fda70f
Billy Gene Ridgeway <37519033+Billoncho@users.noreply.github.com>
2019-08-04 11:53:54 +0800

    Add files via upload

---
+++ MathHomework.py
@@ -0,0 +1,18 @@
+# MathHomework.py
+# Billy Ridgeway
+# This program will solve your math problems.
+
+# Ask the user to enter a math problem or enter 'q' to quit.
+problem = input("Enter a math problem, or 'q' to quit: ")
+
+# Keep going until the user enters 'q' to quit.
+while (problem != "q"):
+
+    # Show the problem, and the answer using eval().
+    print("The answer to ", problem, "is:", eval(problem) )
+
+    #Ask for another math problem.
+    problem = input("Enter another math problem, or 'q' to quit: ")
+
+    # This while loop will keep going until you enter 'q' to quit.
+   
